174b5a0e64e8b7bd22d27e12ce489360b580618c
Matsdd <Matsdoornhein04@gmail.com>
2023-09-25 15:08:26 +0200

    User database updated & login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,23 @@
+@extends('layouts.app')
 
-{{--@extends(['layout.blade.php']);--}}
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>3x3</title>
-</head>
-<body>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<header><h1>hello world</h1></header>
-<main></main>
-<footer></footer>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-</body>
-</html>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
